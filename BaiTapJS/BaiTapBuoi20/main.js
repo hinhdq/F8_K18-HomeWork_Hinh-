39853a51b6a89d08cb79116494295d36fd8b6cae
hinh <hinhdq@fullstack.edu.vn>
2025-12-29 11:21:47 +0700

    jsb20

---
+++ BaiTapJS/BaiTapBuoi20/main.js
@@ -0,0 +1,77 @@
+// p1
+
+// 1
+    var name = 'Duong Quoc Hinh';
+    var age = 21;
+    var isStudent = true;
+
+    console.log('Name:'+ name)
+    console.log('Age:'+ age)
+    console.log('Is student:'+ isStudent)
+
+// 2
+
+    let a = 5;
+    let b = 10;
+
+    a = String(a)
+    b = Number(b)
+
+    console.log(a,b)
+
+// p2
+
+    console.log('const khong the gan lai gia tri moi, con let thi co the thay doi gia tri thoai mai')
+    console.log('const nen dung khi gia tri bien,tham so khong thay doi suot chuong trinh')
+    console.log('doan code sau sai vi const khong thay doi suot chuong trinh')
+
+// p3
+    let a1 = 100;
+    let b1 = 100;
+    let c1 = true;
+    let d1 = [1 , 2 , 3];
+    let e1 = {name:'An', age:20}
+    let f1 = null;
+    let g1 = undefined;
+
+    console.log(typeof a1, typeof b1, typeof c1, typeof d1, typeof e1, typeof f1, typeof g1);
+
+    const Students = {
+        name: 'Duong Quoc Hinh',
+        age: 21,
+        scores: [24 , 4 ,2004]
+    }
+
+    console.log(Students)
+
+// p4
+
+    let a4 = '1000';
+    a4 = Number(a4);
+    console.log(a4, typeof a4);
+
+    let b4 = 1000;
+    b4 = String(b4);
+    console.log(b4, typeof b4);
+
+    let c4 = true;
+    c4 = String(c4);
+    console.log(c4, typeof c4);
+
+// p5
+
+    console.log('false')
+    console.log('true')
+    console.log('false')
+    console.log('true')
+    console.log('false')
+    console.log('true')
+
+// p6
+
+    const Numbers = [1, 2, 3, 4, 5];
+
+    console.log(Numbers[0])
+    console.log(Numbers[4])
+
+// vi khi do a copy dia chi tham chieu toi vung nho chua doi tuong number
