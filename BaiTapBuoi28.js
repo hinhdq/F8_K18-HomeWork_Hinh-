069b92d4d0acc480cfec5fdefddeec090e92c62a
hinhdq <hinhdq@fullstack.edu.vn>
2026-01-31 17:55:49 +0700

    js28

---
+++ BaiTapBuoi28.js
@@ -0,0 +1,71 @@
+// EX 1
+
+interface PartTimeI {
+    id: number;
+    name: string;
+    salary: number;
+    hoursWorked: number;
+}
+
+const partTime: PartTimeI = {
+    id: 2,
+    name: "Binh",
+    salary: 20,
+    hoursWorked: 40
+};
+
+const updatePartTime: PartTimeI = {
+    ...partTime,
+    hoursWorked: 45
+};
+
+console.log(updatePartTime);
+
+// Lesson 2
+
+interface Employee {
+    id: number;
+    name: string;
+    salary: number;
+    getSalary(): number;
+}
+
+class FullTimeEmployee implements Employee {
+    constructor(
+        public id: number,
+        public name: string,
+        public salary: number
+    ) {}
+
+    getSalary(): number {
+        return this.salary;
+    }
+}
+
+class PartTimeEmployee implements Employee {
+    constructor(
+        public id: number,
+        public name: string,
+        public salary: number,
+        public hoursWorked: number
+    ) {}
+
+    getSalary(): number {
+        return this.salary * this.hoursWorked;
+    }
+}
+
+const calculateTotalSalary = (employees: Employee[]): number => {
+    let sum = 0;
+
+    for (const employee of employees) {
+        sum += employee.getSalary();
+    }
+
+    return sum;
+};
+
+const fullTimeEmployee: Employee = new FullTimeEmployee(1, "Duong", 24000);
+const partTimeEmployee: Employee = new PartTimeEmployee(2, "Hinh", 4000, 24);
+
+console.log(calculateTotalSalary([fullTimeEmployee, partTimeEmployee]));
